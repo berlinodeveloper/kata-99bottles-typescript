0cb7ef67e663b1233c5976ba854f1bd44eddc0eb
Vito Alessandro Carella <berlinodeveloper@gmail.com>
2024-12-16 16:36:24 +0100

    mock four line implementation

--- test/Song99Bottles.test.ts
+++ test/Song99Bottles.test.ts
@@ -8,6 +8,7 @@ describe("Song99Bottles Test Suite", function (): void {
       "99 bottles of beer on the wall, 99 bottles of beer.",
       "Take one down and pass it around, 98 bottles of beer on the wall.",
       "98 bottles of beer on the wall, 98 bottles of beer.",
+      "Take one down and pass it around, 97 bottles of beer on the wall.",
     ]);
   });
 });
