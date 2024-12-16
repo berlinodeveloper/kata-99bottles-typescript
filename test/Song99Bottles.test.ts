bf5e5c371c0f3b8285a5f950ed05b5285ea97c65
Vito Alessandro Carella <berlinodeveloper@gmail.com>
2024-12-16 16:22:36 +0100

    rename test suite

--- test/Song99Bottles.test.ts
+++ test/Song99Bottles.test.ts
@@ -1,14 +1,11 @@
 import Song99Bottles from "../src/Song99Bottles";
 const Song99BottlesClass = new Song99Bottles();
 
-describe('sample test set', function (): void {
-
-  it('sample test', (): void => {
-
+describe("Song99Bottles Test Suite", function (): void {
+  it("sample test", (): void => {
     var result: boolean = Song99BottlesClass.handle();
 
     var expected = true;
     expect(result).toBe(expected);
   });
-
 });
