b9eeae7dd1be605a683d122ac036c5b19990a5ba
Vito Alessandro Carella <berlinodeveloper@gmail.com>
2024-12-16 16:26:19 +0100

    rename test

--- test/Song99Bottles.test.ts
+++ test/Song99Bottles.test.ts
@@ -2,7 +2,7 @@ import Song99Bottles from "../src/Song99Bottles";
 const Song99BottlesClass = new Song99Bottles();
 
 describe("Song99Bottles Test Suite", function (): void {
-  it("return lyrics", (): void => {
+  it("prints lyrics", (): void => {
     let result: string[] = Song99BottlesClass.printLyrics();
     expect(result).toEqual([]);
   });
