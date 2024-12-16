01636a04d4fe059820c3cafd35cc7be3b7776282
Vito Alessandro Carella <berlinodeveloper@gmail.com>
2024-12-16 17:38:16 +0100

    fake last 2 lines

--- src/Song99Bottles.ts
+++ src/Song99Bottles.ts
@@ -1,6 +1,6 @@
 class Song99Bottles {
   getLyrics(): string[] {
-    return [...new Array(99)].flatMap((_, i) => {
+    return [...new Array(100)].flatMap((_, i) => {
       const bottlesNumber = 99 - i;
       if (bottlesNumber === 2)
         return [
@@ -12,6 +12,12 @@ class Song99Bottles {
         return [
           "1 bottle of beer on the wall, 1 bottle of beer.",
           "Take one down and pass it around, no more bottles of beer on the wall.",
+        ];
+
+      if (bottlesNumber === 0)
+        return [
+          "No more bottles of beer on the wall, no more bottles of beer.",
+          "Go to the store and buy some more, 99 bottles of beer on the wall.",
         ];
 
       return [
