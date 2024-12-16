02ca35f1e988b6d711d4c9b509b100b75bdc3af2
Vito Alessandro Carella <berlinodeveloper@gmail.com>
2024-12-16 16:33:10 +0100

    mock third line of lyrics

--- src/Song99Bottles.ts
+++ src/Song99Bottles.ts
@@ -3,6 +3,7 @@ class Song99Bottles {
     return [
       "99 bottles of beer on the wall, 99 bottles of beer.",
       "Take one down and pass it around, 98 bottles of beer on the wall.",
+      "98 bottles of beer on the wall, 98 bottles of beer.",
     ];
   }
 }
--- test/Song99Bottles.test.ts
+++ test/Song99Bottles.test.ts
@@ -7,6 +7,7 @@ describe("Song99Bottles Test Suite", function (): void {
     expect(result).toEqual([
       "99 bottles of beer on the wall, 99 bottles of beer.",
       "Take one down and pass it around, 98 bottles of beer on the wall.",
+      "98 bottles of beer on the wall, 98 bottles of beer.",
     ]);
   });
 });
