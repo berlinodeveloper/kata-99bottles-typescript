b1d24cf5fac310e7d9feab8037bfdc5c84a4754f
Vito Alessandro Carella <berlinodeveloper@gmail.com>
2024-12-16 17:52:26 +0100

    implements last 2 lines

--- src/Song99Bottles.ts
+++ src/Song99Bottles.ts
@@ -3,21 +3,19 @@ class Song99Bottles {
     return [...new Array(100)].flatMap((_, i) => {
       const bottlesNumber = 99 - i;
 
-      if (bottlesNumber === 0)
-        return [
-          "No more bottles of beer on the wall, no more bottles of beer.",
-          "Go to the store and buy some more, 99 bottles of beer on the wall.",
-        ];
-
       return [
-        `${bottlesNumber} ${
+        `${bottlesNumber === 0 ? "No more" : bottlesNumber} ${
           bottlesNumber === 1 ? "bottle" : "bottles"
-        } of beer on the wall, ${bottlesNumber} ${
-          bottlesNumber === 1 ? "bottle" : "bottles"
-        } of beer.`,
-        `Take one down and pass it around, ${
-          bottlesNumber === 1 ? "no more" : bottlesNumber - 1
-        } ${bottlesNumber === 2 ? "bottle" : "bottles"} of beer on the wall.`,
+        } of beer on the wall, ${
+          bottlesNumber === 0 ? "no more" : bottlesNumber
+        } ${bottlesNumber === 1 ? "bottle" : "bottles"} of beer.`,
+        bottlesNumber === 0
+          ? "Go to the store and buy some more, 99 bottles of beer on the wall."
+          : `Take one down and pass it around, ${
+              bottlesNumber === 1 ? "no more" : bottlesNumber - 1
+            } ${
+              bottlesNumber === 2 ? "bottle" : "bottles"
+            } of beer on the wall.`,
       ];
     });
   }
