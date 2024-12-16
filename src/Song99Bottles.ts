f8ae9e37023c8dd3ea75ce53b5760c286a0a4a3f
Vito Alessandro Carella <berlinodeveloper@gmail.com>
2024-12-16 16:45:43 +0100

    mock 5 and 6 line

--- src/Song99Bottles.ts
+++ src/Song99Bottles.ts
@@ -5,6 +5,8 @@ class Song99Bottles {
       "Take one down and pass it around, 98 bottles of beer on the wall.",
       "98 bottles of beer on the wall, 98 bottles of beer.",
       "Take one down and pass it around, 97 bottles of beer on the wall.",
+      "97 bottles of beer on the wall, 97 bottles of beer.",
+      "Take one down and pass it around, 96 bottles of beer on the wall.",
     ];
   }
 }
